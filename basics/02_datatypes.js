87b3d2ac586921e2c51c02e5bff8b6f816d57695
ojastayal17 <ojastayal35@gmail.com>
2025-06-04 13:02:23 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  we are using nodejs, not browser
+
+let name="ojas"
+let age=19
+let isLoggedIn=false
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+//undefined
+// symbol => unique
+
+
+//object 
+
+console.log(typeof "ojas")
+console.log(typeof age)
+console.log(typeof null) //object 
+console.log(typeof undefined) //undefined
+
