39c513066878840485502842fd1d02d6d348a335
ojastayal17 <ojastayal35@gmail.com>
2025-06-17 13:20:27 +0000

    conversion of data types

---
+++ basics/03_conversion.js
@@ -0,0 +1,24 @@
+let score="33"
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+
+// "33"=>33
+//"33abc"=>NAN
+//true=1 false=0
+
+let isLogIn=1
+
+let booleanIsLogIn=Boolean(isLogIn)
+
+console.log( booleanIsLogIn);
+ // 0=> true 1=> false
+ //"" => false
+ //"ojas" => true
+
+ let somenumber=33
+
+ let stringNumber=String(somenumber)
+ console.log( stringNumber);
+ console.log( typeof stringNumber);
